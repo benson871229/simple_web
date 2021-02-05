943e2b539ceb710909e2aa6cd15fa81d43c8fe89
benson871229 <benson80707@gmail.com>
2021-02-05 10:00:45 +0800

    simple_web

---
+++ web.py
@@ -0,0 +1,21 @@
+import flask
+from flask import Flask, render_template,  request, redirect, g, send_file
+from flask.helpers import url_for
+app = flask.Flask(__name__)
+
+
+
+
+@app.route('/')
+def nighthawk():
+    return render_template("WEB.html")
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host="10.0.5.10")
+
+
+
+
+
+
